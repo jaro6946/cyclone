8da7f43f5cea5313c38917de070a8053e65bbea3
RPI Jaro6946 <jaro6946@colorado.edu>
2018-01-30 02:00:34 +0000

    bug fixes

--- pollingCyclone.py
+++ pollingCyclone.py
@@ -2,6 +2,28 @@ import RPi.GPIO as GPIO
 import time
 import math
 
+GPIO.cleanup()
+
+class cycleTimer(object):
+	
+	def __init__(self, initTime, cycleLength,actionsPerCycle):
+		self.actionTime=cycleLength/actionsPerCycle
+		self.cycleLength=cycleLength
+		self.initTime=initTime
+		self.actionsPerCycle=actionsPerCycle
+		
+		
+	def cycleNum(self):
+
+		self.currentTime=time.time()
+		self.cyclePer=(self.currentTime-self.initTime)/self.cycleLength
+		self.cycleTime=(self.cyclePer-math.floor(self.cyclePer))*self.cycleLength
+		self.cycleNumber=math.floor(self.cycleTime/self.actionTime)
+		return self.cycleNumber
+
+
+
+
 GPIO.setmode(GPIO.BCM)
 
 buttonInput=13
@@ -11,7 +33,9 @@ ledNumbers=(19,20,21,22,23,24,25)
 GPIO.setup(buttonInput, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 GPIO.setup(ledNumbers, GPIO.OUT)
-Timer1=cycleTimer(time.time(), cycleLength,length(ledNumbers))
+cycleLength=.3
+
+Timer1=cycleTimer(time.time(), cycleLength,len(ledNumbers))
 
 while True:
 	whichLED=Timer1.cycleNum()
@@ -20,13 +44,12 @@ while True:
 	GPIO.output(offLEDs, GPIO.LOW)
 	GPIO.output(ledNumbers[whichLED], GPIO.HIGH)
 	
-	if(GPIO.input(buttonPress) == 1):
+	if(GPIO.input(buttonInput) == 1):
 		GPIO.output(ledNumbers, GPIO.LOW)
-		if whichLED == math.floor(length(ledNumbers)/2):
+		if whichLED == math.floor(len(ledNumbers)/2):
 			GPIO.output(ledNumbers[whichLED], GPIO.HIGH)
 			break
-		else:
-			GPIO.output(ledNumbers[0], GPIO.HIGH)
+		
 
 
 
@@ -37,18 +60,6 @@ while True:
 
 GPIO.cleanup()
 
-
-class cycleTimer(object):
-	
-	def __init__(self, initTime,cycleLength,actionsPerCycle):
-		self.actionTime=self.cycleLength/self.actionsPerCycle
-	
-	def cycleNum(self):
-
-		self.currentTime=time.time()
-		self.cycleTime=(self.currentTime-self.initialTime)/self.cycleLength
-		self.cycleNumber=math.floor(self.cycleTime/self.actionTime)
-		return cycleNumber
 
 
 
