81dfa144004e85edba53552e671ad77745ed39af
jaro6946 <jaro6946@colorado.edu>
2018-02-06 19:43:34 -0700

    added rising edge detection for button

--- cyclone.py
+++ cyclone.py
@@ -22,26 +22,19 @@ class cycleTimer(object):
 		self.cycleNumber=math.floor(self.cycleTime/self.actionTime)
 		return self.cycleNumber
 
-	def strobe(self):
+class risingEdge(object)
+	def __init__(self)
+		pass
 
-		self.currentTime=time.time()
-		self.cyclePercent=(self.currentTime-self.initTime)/self.cycleLength
-		self.cyclePercent=self.cyclePercent-math.floor(self.cyclePercent)
-                
-		if self.cyclePercent > .5:
-			#print(self.cyclePercent)
-			self.cyclePercentMod=(100-self.cyclePercent*100)/100
-			#print(self.cyclePercentMod)
-			#print((100-self.cyclePercent*100)/100)
+	def checkForEdge(self,signal)
+		self.signal=signal
+		
+		if self.signal==1+self.signalPast
+			self.signalPast=self.signal
+			return True
 		else:
-			self.cyclePercentMod=self.cyclePercent
-		#print(self.cyclePercent)
-		self.cycleTime=self.cyclePercentMod*self.cycleLength
-		#print(self.cyclePercent)
-		self.cycleNumber=math.floor(self.cycleTime/self.actionTime)
-		return self.cycleNumber*2
-
-
+			self.signalPast=self.signal
+			return False
 
 GPIO.setmode(GPIO.BCM)
 
@@ -50,11 +43,13 @@ ledNumbers=(18,20,21,22,23,24,25)
 
 
 GPIO.setup(buttonInput, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
 GPIO.setup(ledNumbers, GPIO.OUT)
+
 cycleLength=.3
 
 Timer1=cycleTimer(time.time(), cycleLength,len(ledNumbers))
+checkEdge=risingEdge()
+
 try:
 	while True:
 		whichLED=Timer1.cycleNum()
@@ -63,38 +58,26 @@ try:
 		GPIO.output(offLEDs, GPIO.LOW)
 		GPIO.output(ledNumbers[whichLED], GPIO.HIGH)
 		
-		if(GPIO.input(buttonInput) == 1):
-			GPIO.output(ledNumbers, GPIO.LOW)
-			if whichLED == math.floor(len(ledNumbers)/2):
-				GPIO.output(ledNumbers[whichLED], GPIO.HIGH)
+		if(checkEdge(GPIO.input(buttonInput)) == 1):
+			time.sleep(.1)
+			if whichLED == 4:
+				GPIO.output(ledNumbers, GPIO.LOW)
 				break
 
-	strobe1=cycleTimer(time.time(), 3,100)
 	timer2=cycleTimer(time.time(), 12,2)
 	GPIO.cleanup()
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setup(22, GPIO.OUT)
-	dutyCyclePast=-1
 
 	pwm = GPIO.PWM(22, 100)
 	pwm.start(25)
-	while True:
-		if timer2.cycleNum()==1:
-			break
-
-
+	
+	while timer2.cycleNum()<1:
+		pass #pass does nothing, but python requirws a place holder in this instance
 
 except:
 	GPIO.cleanup()
 	raise
-
-
-
-
-
-
-#pwm = GPIO.PWM(18, 1000)
-#pwm.start(50)
 
 GPIO.cleanup()
 
